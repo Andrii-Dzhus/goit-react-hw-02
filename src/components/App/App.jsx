dc53ccfbff5db484b21e6c9b9eac3a1686979740
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-08-14 21:52:05 +0300

    21:50

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,30 @@
 import Feedback from "../Feedback/Feedback";
 import Options from "../Options/Options";
 import Description from "../Description/Description";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Notification from "../Notification/Notification";
 
 export default function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedback, setFeedback] = useState(() => {
+    const saveFeedback = window.localStorage.getItem("feedback");
+    return saveFeedback !== null
+      ? JSON.parse(saveFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
   });
+
+  useEffect(() => {
+    window.localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
   const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
 
   const updateFeedback = feedbackType => {
-    setFeedback(prevFeedback => ({
-      ...prevFeedback,
-      [feedbackType]: prevFeedback[feedbackType] + 1,
-    }));
+    setFeedback({
+      ...feedback,
+      [feedbackType]: feedback[feedbackType] + 1,
+    });
   };
 
   const resetClicks = () => {
